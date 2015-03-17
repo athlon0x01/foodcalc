640fdd05fa8a842a461291392f63614224169bb5
Anton Borovyk <Anton.Borovyk@medhost.com>
2015-03-17 16:48:48 +0200

    Inintial commit

---
+++ PostgreDB.sql
@@ -0,0 +1,79 @@
+--PostgeSQL implementation of Food Calculator
+
+--product related tables
+CREATE TABLE product_categories (
+	id smallserial PRIMARY KEY,
+	name varchar(64) NOT NULL
+);
+
+CREATE TABLE products (
+	id serial PRIMARY KEY,
+	name varchar(64) NOT NULL,
+	category smallint REFERENCES product_category,
+	calorific real DEFAULT 0.0,
+	proteins real DEFAULT 0.0,
+	fats real DEFAULT 0.0,
+	carbs real DEFAULT 0.0,
+	defweight real DEFAULT 0.0
+);
+
+--dish related tables
+CREATE TABLE dish_categories (
+	id smallserial PRIMARY KEY,
+	name varchar(64) NOT NULL
+);
+
+CREATE TABLE dishes (
+	id serial PRIMARY KEY,
+	name varchar(64) NOT NULL,
+	description varchar(1024),
+	category smallint REFERENCES dish_category,
+	calorific real DEFAULT 0.0,
+	proteins real DEFAULT 0.0,
+	fats real DEFAULT 0.0,
+	carbs real DEFAULT 0.0,
+	weight real DEFAULT 0.0
+);
+
+CREATE TABLE dish_products (
+	id serial PRIMARY KEY,
+	dish integer REFERENCES dishes,
+	product integer REFERENCES products,
+	weight real NOT NULL
+);
+
+--product layout related tables
+CREATE TABLE meals (
+	id smallserial PRIMARY KEY,
+	name varchar(64) NOT NULL
+);
+
+CREATE TABLE grocery_layouts (
+	id serial PRIMARY KEY,
+	name varchar(64) NOT NULL,
+	members smallint NOT NULL,
+	duration smallint,
+	description varchar(2048),
+	calorific real DEFAULT 0.0,
+	proteins real DEFAULT 0.0,
+	fats real DEFAULT 0.0,
+	carbs real DEFAULT 0.0,
+	weight real DEFAULT 0.0
+);
+
+CREATE TABLE layout_days (
+	id serial PRIMARY KEY,
+	day_index smallint NOT NULL,
+	layout integer REFERENCES products_layouts,
+	day_date date,
+	description varchar(1024)
+);
+
+CREATE TABLE grocery_layouts_items (
+	id serial PRIMARY KEY,
+	layout_day integer REFERENCES layout_days,
+	meal smallint REFERENCES meals,
+	dish integer REFERENCES dishes,
+	product integer REFERENCES products,
+	weight real NOT NULL
+);
---
+++ domain-tier/src/main/java/com/outdoor/foodcalc/domain/repository/IProductRepo.java
@@ -0,0 +1,9 @@
+package com.outdoor.foodcalc.domain.repository;
+
+/**
+ * <description>
+ *
+ * @author Anton Borovyk
+ */
+public class IProductRepo {
+}
