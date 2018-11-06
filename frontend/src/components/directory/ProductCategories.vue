0603146d76394a7e95fd24cf022a43666cb4c4f1
Anton Borovyk <Anton.Borovyk@dataart.com>
2018-11-06 14:49:49 +0200

    UI validation added

--- frontend/src/components/directory/ProductCategories.vue
+++ frontend/src/components/directory/ProductCategories.vue
@@ -10,19 +10,23 @@
       <div v-for="(category, index) in categories" :key="category.id" class="row">
         <template v-if="editCategory === category.id">
           <div class="col-sm-10" style="text-align: left">
-            <input v-on:keyup.13="updateCategory(category)" v-model="category.name" style="width: 100%"/>
+            <input v-validate="'required'"  v-model="category.name" name="category.name"
+                   v-on:keyup.13="updateCategory(category)" style="width: 100%"/>
           </div>
           <div class="col-sm-1">
             <b-button variant="outline-success" size="sm" v-on:click="updateCategory(category)">Update</b-button>
           </div>
           <div class="col-sm-1">
-            <b-button variant="outline-danger" size="sm" v-on:click="editCategory = null">Cancel</b-button>
+            <b-button variant="outline-danger" size="sm" v-on:click="cancelEdit(category)">Cancel</b-button>
+          </div>
+          <div v-show="errors.has('category.name')" class="alert" style="margin-top: 5px">
+            <span>{{errors.first('category.name')}}</span>
           </div>
         </template>
         <template v-else>
           <div class="col-sm-10 text-left"><i>{{category.name}}</i></div>
           <div class="col-sm-1">
-            <b-button variant="outline-success" size="sm" v-on:click="editCategory = category.id">Edit</b-button>
+            <b-button variant="outline-success" size="sm" v-on:click="startEdit(category)">Edit</b-button>
           </div>
           <div class="col-sm-1">
             <b-button variant="outline-danger" size="sm" v-on:click="deleteCategory(category.id, index)">Delete</b-button>
@@ -40,9 +44,20 @@
 
     <!--Add new category section-->
     <b-button variant="link" size="sm" v-on:click="addMode = !addMode">Add new category</b-button>
-    <div v-if="addMode !== undefined && addMode">
-      <input v-on:keyup.13="addCategory()" v-model="newName"/>
-      <b-button variant="outline-success" size="sm" v-on:click="addCategory()">Add</b-button>
+    <div v-if="addMode !== undefined && addMode" class="container">
+      <div class="row">
+        <div class="col-sm-10">
+          <input v-validate="'required'" v-model="newCategory" name="newCategory"
+                 v-bind:class="{ validationError: errors.has('newCategory')}"
+                 v-on:keyup.13="addCategory()" placeholder="Enter new category here.." style="width: 100%"/>
+        </div>
+        <div class="col-sm-1">
+          <b-button variant="outline-success" size="sm" v-on:click="addCategory()">Add</b-button>
+        </div>
+      </div>
+      <div v-show="errors.has('newCategory')" class="row alert" style="margin-top: 5px">
+        <span class="col-sm-10">{{errors.first('newCategory')}}</span>
+      </div>
     </div>
   </div>
 </template>
@@ -55,8 +70,9 @@ export default {
   data () {
     return {
       addMode: false,
-      newName: '',
+      newCategory: '',
       editCategory: null,
+      oldName: null,
       categories: [],
       errorMessage: null
     }
@@ -73,43 +89,71 @@ export default {
       }
     },
 
+    clearErrors () {
+      this.errorMessage = null
+      this.errors.clear()
+    },
+
     addCategory () {
-      let newCategory = {
-        name: this.newName
-      }
-      axios.post('/api/product-categories', newCategory)
-        .then(response => {
-          this.categories.push(response.data)
-          this.addMode = false
-          this.newName = ''
-          this.errorMessage = null
-        })
-        .catch(e => {
-          this.getErrorMessage(e, 'Failed to add new Product Category ' + this.newName)
-        })
+      this.$validator.validateAll().then((result) => {
+        if (result) {
+          let newCategory = {
+            name: this.newCategory
+          }
+          axios.post('/api/product-categories', newCategory)
+            .then(response => {
+              this.categories.push(response.data)
+              this.addMode = false
+              this.newCategory = ''
+              this.clearErrors()
+            })
+            .catch(e => {
+              this.getErrorMessage(e, 'Failed to add new Product Category ' + this.newCategory)
+            })
+        } else {
+          console.log('Couldn\'t add new category due to validation errors')
+        }
+      })
     },
 
     updateCategory (category) {
-      axios.put('/api/product-categories/' + category.id, category)
-        .then(() => {
-          this.editCategory = null
-          this.errorMessage = null
-        })
-        .catch(e => {
-          this.getErrorMessage(e, 'Failed to update Product Category ' + category.name)
-          this.editCategory = null
-        })
+      this.$validator.validateAll().then((result) => {
+        if (result) {
+          axios.put('/api/product-categories/' + category.id, category)
+            .then(() => {
+              this.editCategory = null
+              this.clearErrors()
+            })
+            .catch(e => {
+              this.getErrorMessage(e, 'Failed to update Product Category ' + category.name)
+              this.editCategory = null
+            })
+        } else {
+          console.log('Couldn\'t update category due to validation errors')
+        }
+      })
     },
 
     deleteCategory (id, index) {
       axios.delete('/api/product-categories/' + id)
         .then(() => {
           this.categories.splice(index, 1)
-          this.errorMessage = null
+          this.clearErrors()
         })
         .catch(e => {
           this.getErrorMessage(e, 'Failed to delete Product Category ' + this.categories[index].name)
         })
+    },
+
+    startEdit (category) {
+      this.editCategory = category.id
+      this.oldName = category.name
+    },
+
+    cancelEdit (category) {
+      this.editCategory = null
+      category.name = this.oldName
+      this.clearErrors()
     }
   },
 
@@ -118,7 +162,7 @@ export default {
     axios.get('/api/product-categories')
       .then(response => {
         this.categories = response.data
-        this.errorMessage = null
+        this.clearErrors()
       })
       .catch(e => {
         this.getErrorMessage(e, 'Failed to load Product categories...')
@@ -128,4 +172,7 @@ export default {
 </script>
 
 <style scoped>
+  .validationError {
+    background-color: orange;
+  }
 </style>
