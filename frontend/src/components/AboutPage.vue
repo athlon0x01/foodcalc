7fcd42bcb3ad7afc8da9f236c5ddab3a6f02de87
Anton Borovyk <aborovyk@dataart.com>
2019-03-15 14:00:23 +0200

    nodejs dependencies updated

--- frontend/src/components/AboutPage.vue
+++ frontend/src/components/AboutPage.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h2 align="center">Outdoor Food Calculator About page</h2>
-    <p align="center">(c) Anton Borovyk&lt;athlon0x01@gmail.com&gt;, 2018</p>
+    <p align="center">(c) Anton Borovyk&lt;athlon0x01@gmail.com&gt;, 2019</p>
   </div>
 </template>
 
