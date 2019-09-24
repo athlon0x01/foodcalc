77829316956f6987399fde5c59e490e4847d5035
Anton Borovyk <aborovyk@dataart.com>
2019-09-24 11:23:23 +0300

    DirectoryList component added

---
+++ frontend/src/components/directory/DirectoryList.vue
@@ -0,0 +1,147 @@
+<template>
+  <div>
+    <h2 class="directory-header">{{title}}</h2>
+
+    <!--List-->
+    <div v-if="items.length > 0" class="container">
+      <div class="row headerRow">
+        <div class="col-sm-10"><strong>Name</strong></div>
+      </div>
+      <div v-for="item in items" :key="item.id">
+        <directory-list-item v-bind:item-data="item" v-on:update="updateItem" v-on:remove="removeItem"/>
+      </div>
+    </div>
+    <div v-if="items.length === 0 && errorMessage === null">
+      <p><i>{{noItemsMsg}}</i></p>
+    </div>
+    <!--Errors output-->
+    <div v-if="errorMessage !== null" class="alert">
+      <p>{{errorMessage}}</p>
+    </div>
+
+    <!--Add new item section-->
+    <b-button variant="link" size="sm" v-on:click="addMode = !addMode">Add new</b-button>
+    <div v-if="addMode !== undefined && addMode" class="container">
+      <directory-list-new-item v-on:addNew="addItem"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import DirectoryListItem from 'src/components/directory/DirectoryListItem'
+import DirectoryListNewItem from 'src/components/directory/DirectoryListNewItem'
+
+export default {
+  name: 'DirectoryList',
+
+  components: {DirectoryListNewItem, DirectoryListItem},
+
+  props: {
+    endpointUrl: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    noItemsMsg: {
+      type: String,
+      required: true
+    }
+  },
+
+  data () {
+    return {
+      addMode: false,
+      items: [],
+      errorMessage: null
+    }
+  },
+
+  methods: {
+    getErrorMessage (error, defaultMessage) {
+      if (error.response !== undefined && error.response.data !== undefined &&
+        (typeof error.response.data === 'string' || error.response.data instanceof String)) {
+        this.errorMessage = error.response.data
+      } else {
+        console.log(error)
+        this.errorMessage = defaultMessage
+      }
+    },
+
+    clearErrors () {
+      this.errorMessage = null
+      this.errors.clear()
+    },
+
+    addItem (newItem) {
+      this.$validator.validateAll().then((result) => {
+        if (result) {
+          let newItemObject = {
+            name: newItem
+          }
+          axios.post(this.endpointUrl, newItemObject)
+            .then(response => {
+              this.items.push(response.data)
+              this.addMode = false
+              this.clearErrors()
+            })
+            .catch(e => {
+              this.getErrorMessage(e, 'Failed to add ' + newItem)
+            })
+        } else {
+          console.log('Couldn\'t add new item due to validation errors')
+        }
+      })
+    },
+
+    updateItem (item) {
+      this.$validator.validateAll().then((result) => {
+        if (result) {
+          axios.put(this.endpointUrl + item.id, item)
+            .then(() => {
+              let index = this.items.findIndex(it => it.id === item.id)
+              this.items[index] = item
+              this.clearErrors()
+            })
+            .catch(e => {
+              this.getErrorMessage(e, 'Failed to update ' + item.name)
+            })
+        } else {
+          console.log('Couldn\'t update item due to validation errors')
+        }
+      })
+    },
+
+    removeItem (id) {
+      axios.delete(this.endpointUrl + id)
+        .then(() => {
+          this.items = this.items.filter(item => item.id !== id)
+          this.clearErrors()
+        })
+        .catch(e => {
+          let item = this.items.find(item => item.id === id)
+          this.getErrorMessage(e, 'Failed to delete ' + item.name)
+        })
+    }
+  },
+
+  mounted () {
+    // load content on page init
+    axios.get(this.endpointUrl)
+      .then(response => {
+        this.items = response.data
+        this.clearErrors()
+      })
+      .catch(e => {
+        this.getErrorMessage(e, 'Failed to load items...')
+      })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- frontend/src/components/directory/DishCategories.vue
+++ frontend/src/components/directory/DishCategories.vue
@@ -1,132 +1,23 @@
 <template>
-  <div>
-    <h2 class="directory-header">Dish categories:</h2>
-
-    <!--Categories table-->
-    <div v-if="categories.length > 0" class="container">
-      <div class="row headerRow">
-        <div class="col-sm-10"><strong>Name</strong></div>
-      </div>
-      <div v-for="category in categories" :key="category.id">
-        <directory-list-item v-bind:item-data="category" v-on:update="updateCategory" v-on:remove="removeCategory"/>
-      </div>
-    </div>
-    <div v-if="categories.length === 0 && errorMessage === null">
-      <p><i>No Categories loaded</i></p>
-    </div>
-    <!--Errors output-->
-    <div v-if="errorMessage !== null" class="alert">
-      <p>{{errorMessage}}</p>
-    </div>
-
-    <!--Add new item section-->
-    <b-button variant="link" size="sm" v-on:click="addMode = !addMode">Add new dish category</b-button>
-    <div v-if="addMode !== undefined && addMode" class="container">
-      <directory-list-new-item v-on:addNew="addCategory"/>
-    </div>
-  </div>
+  <directory-list v-bind:endpoint-url="url" v-bind:title="title" v-bind:no-items-msg="noItemsMsg"/>
 </template>
 
 <script>
-import axios from 'axios'
-import DirectoryListItem from 'src/components/directory/DirectoryListItem'
-import DirectoryListNewItem from 'src/components/directory/DirectoryListNewItem'
+import DirectoryList from 'src/components/directory/DirectoryList'
 
 export default {
   name: 'DishCategories',
-  components: {DirectoryListNewItem, DirectoryListItem},
+  components: {DirectoryList},
   data () {
     return {
-      addMode: false,
-      categories: [],
-      errorMessage: null
+      url: '/api/dish-categories/',
+      title: 'Dish categories:',
+      noItemsMsg: 'No dish categories yet...'
     }
-  },
-
-  methods: {
-    getErrorMessage (error, defaultMessage) {
-      if (error.response !== undefined && error.response.data !== undefined &&
-        (typeof error.response.data === 'string' || error.response.data instanceof String)) {
-        this.errorMessage = error.response.data
-      } else {
-        console.log(error)
-        this.errorMessage = defaultMessage
-      }
-    },
-
-    clearErrors () {
-      this.errorMessage = null
-      this.errors.clear()
-    },
-
-    addCategory (newItem) {
-      this.$validator.validateAll().then((result) => {
-        if (result) {
-          let newItemObject = {
-            name: newItem
-          }
-          axios.post('/api/dish-categories', newItemObject)
-            .then(response => {
-              this.categories.push(response.data)
-              this.addMode = false
-              this.clearErrors()
-            })
-            .catch(e => {
-              this.getErrorMessage(e, 'Failed to add new Dish Category ' + this.newCategory)
-            })
-        } else {
-          console.log('Couldn\'t add new category due to validation errors')
-        }
-      })
-    },
-
-    updateCategory (category) {
-      this.$validator.validateAll().then((result) => {
-        if (result) {
-          axios.put('/api/dish-categories/' + category.id, category)
-            .then(() => {
-              let index = this.categories.findIndex(cat => cat.id === category.id)
-              this.categories[index] = category
-              this.clearErrors()
-            })
-            .catch(e => {
-              this.getErrorMessage(e, 'Failed to update Dish Category ' + category.name)
-            })
-        } else {
-          console.log('Couldn\'t update category due to validation errors')
-        }
-      })
-    },
-
-    removeCategory (id) {
-      axios.delete('/api/dish-categories/' + id)
-        .then(() => {
-          this.categories = this.categories.filter(category => category.id !== id)
-          this.clearErrors()
-        })
-        .catch(e => {
-          let category = this.categories.find(category => category.id === id)
-          this.getErrorMessage(e, 'Failed to delete Dish Category ' + category.name)
-        })
-    }
-  },
-
-  mounted () {
-    // load dish categories on page init
-    axios.get('/api/dish-categories')
-      .then(response => {
-        this.categories = response.data
-        this.clearErrors()
-      })
-      .catch(e => {
-        this.getErrorMessage(e, 'Failed to load Dish categories...')
-      })
   }
 }
 </script>
 
 <style scoped>
-  .validationError {
-    background-color: orange;
-  }
+
 </style>
