66b5d1f12e70dddbbc6e84a43a2c250337969ca9
oborovyk <pennerall@gmail.com>
2022-05-09 14:12:41 +0300

    IProductRepo created

--- domain-tier/src/main/java/com/outdoor/foodcalc/domain/repository/product/IProductRepo.java
+++ domain-tier/src/main/java/com/outdoor/foodcalc/domain/repository/product/IProductRepo.java
@@ -3,11 +3,12 @@ package com.outdoor.foodcalc.domain.repository.product;
 import com.outdoor.foodcalc.domain.model.product.Product;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Product repository responsible for {@link Product} persistence.
  *
- * @author Anton Borovyk
+ * @author Anton Borovyk & Olga Borovyk
  */
 public interface IProductRepo {
 
@@ -24,4 +25,44 @@ public interface IProductRepo {
      * @return products number
      */
     long countProductsInCategory(long categoryId);
+
+    /**
+     * Loads {@link Product} object by Id.
+     *
+     * @param id product id
+     * @return loaded product
+     */
+    Optional<Product> getProduct(long id);
+
+    /**
+     * Add new {@link Product}.
+     *
+     * @param product to add
+     * @return auto generated Id
+     */
+    long addProduct(Product product);
+
+    /**
+     * Updates selected {@link Product} with new value.
+     *
+     * @param product to update
+     * @return if product was updated
+     */
+    boolean updateProduct(Product product);
+
+    /**
+     * Deletes selected {@link Product}.
+     *
+     * @param id product to delete
+     * @return if product was deleted
+     */
+    boolean deleteProduct(long id);
+
+    /**
+     * Check if specified {@link Product} exists.
+     *
+     * @param id product to check
+     * @return if product exists
+     */
+    boolean existsProduct(long id);
 }
