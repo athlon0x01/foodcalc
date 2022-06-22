168eb1bccb50547e7a19796bd28dd8624de14f49
Anton Borovyk <athlon0x01@gmail.com>
2022-06-22 13:20:15 +0300

    Java upgrade to 11 version

--- web-tier/src/main/java/com/outdoor/foodcalc/FoodcalcApplication.java
+++ web-tier/src/main/java/com/outdoor/foodcalc/FoodcalcApplication.java
@@ -2,12 +2,15 @@ package com.outdoor.foodcalc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
  * SpringBoot application
  *
  * @author Anton Borovyk.
  */
+//TODO move Swagger to spring doc or open doc
+@EnableWebMvc
 @SpringBootApplication
 public class FoodcalcApplication {
 
