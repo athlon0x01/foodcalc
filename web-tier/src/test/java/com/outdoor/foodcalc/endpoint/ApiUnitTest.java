3b470089b289de3c52284288c614c0c4f2cfefed
oborovyk <pennerall@gmail.com>
2022-09-15 16:22:17 +0300

    ApiUnitTest some fix

--- web-tier/src/test/java/com/outdoor/foodcalc/endpoint/ApiUnitTest.java
+++ web-tier/src/test/java/com/outdoor/foodcalc/endpoint/ApiUnitTest.java
@@ -97,7 +97,8 @@ public abstract class ApiUnitTest {
      */
     public ResultActions post(String url, Object body) throws Exception {
         return postJson(url, body)
-            .andExpect(status().isCreated()).andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8));
+            .andExpect(status().isCreated())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8));
     }
 
     public ResultActions post400(String url, Object body) throws Exception {
