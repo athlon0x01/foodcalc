bf4a0fe8b062d073f6c55775c4458f3dc859dd2c
Olga Borovyk <pennerall@gmail.com>
2023-09-11 16:35:47 +0300

    equals & hashCode added to DishProduct, SimpleDish

--- web-tier/src/main/java/com/outdoor/foodcalc/model/dish/DishProduct.java
+++ web-tier/src/main/java/com/outdoor/foodcalc/model/dish/DishProduct.java
@@ -2,10 +2,24 @@ package com.outdoor.foodcalc.model.dish;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.PositiveOrZero;
+import java.util.Objects;
 
 public class DishProduct {
     @Min(1)
     public long productId;
     @PositiveOrZero
     public float weight;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DishProduct that = (DishProduct) o;
+        return productId == that.productId && Float.compare(that.weight, weight) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(productId, weight);
+    }
 }
