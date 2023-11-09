d31e8b5e08e7bc23b5dc805005abf450e1f61442
Olga Borovyk <pennerall@gmail.com>
2023-11-09 11:16:45 +0200

    MealType uses lombok

--- domain-tier/src/main/java/com/outdoor/foodcalc/domain/model/meal/MealType.java
+++ domain-tier/src/main/java/com/outdoor/foodcalc/domain/model/meal/MealType.java
@@ -1,55 +1,24 @@
 package com.outdoor.foodcalc.domain.model.meal;
 
 import com.outdoor.foodcalc.domain.model.IDomainEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
 /**
  * Meal type, like breakfast, lunch, etc.
  *
  * @author Anton Borovyk
  */
+
+@Data
+@AllArgsConstructor
 public class MealType implements IDomainEntity<MealType> {
 
     private final int typeId;
     private String name;
 
-    public MealType(int typeId, String name) {
-        this.typeId = typeId;
-        this.name = name;
-    }
-
-    public int getTypeId() {
-        return typeId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     @Override
     public boolean sameIdentityAs(MealType other) {
         return typeId == other.typeId;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        MealType mealType = (MealType) o;
-
-        if (typeId != mealType.typeId) return false;
-        return !(name != null ? !name.equals(mealType.name) : mealType.name != null);
-
-    }
-
-    @Override
-    public int hashCode() {
-        int result = typeId;
-        result = 31 * result + (name != null ? name.hashCode() : 0);
-        return result;
     }
 }
