40df536747af82413606b5fe7212ff1c22fcab96
Olga <pennerall@gmail.com>
2023-11-20 14:29:22 +0200

    Domain tier - lombok for ProductCategory, DishCategory, MealType (#47)
    * DishCategory, MealType, ProductCategory uses lombok
    
    * Lombok moved to root pom file

--- domain-tier/src/main/java/com/outdoor/foodcalc/domain/model/dish/DishCategory.java
+++ domain-tier/src/main/java/com/outdoor/foodcalc/domain/model/dish/DishCategory.java
@@ -1,54 +1,24 @@
 package com.outdoor.foodcalc.domain.model.dish;
 
 import com.outdoor.foodcalc.domain.model.IDomainEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
 /**
  * Dish categories, like soups, snacks, hot drinks, etc.
  *
  * @author Anton Borovyk
  */
+
+@Data
+@AllArgsConstructor
 public class DishCategory implements IDomainEntity<DishCategory> {
 
     private final long categoryId;
     private String name;
 
-    public DishCategory(long categoryId, String name) {
-        this.categoryId = categoryId;
-        this.name = name;
-    }
-
-    public long getCategoryId() {
-        return categoryId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     @Override
     public boolean sameIdentityAs(DishCategory other) {
         return categoryId == other.categoryId;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        DishCategory that = (DishCategory) o;
-
-        if (categoryId != that.categoryId) return false;
-        return name != null ? name.equals(that.name) : that.name == null;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = (int) (categoryId ^ (categoryId >>> 32));
-        result = 31 * result + (name != null ? name.hashCode() : 0);
-        return result;
     }
 }
--- domain-tier/src/main/java/com/outdoor/foodcalc/domain/model/product/ProductCategory.java
+++ domain-tier/src/main/java/com/outdoor/foodcalc/domain/model/product/ProductCategory.java
@@ -1,54 +1,24 @@
 package com.outdoor.foodcalc.domain.model.product;
 
 import com.outdoor.foodcalc.domain.model.IDomainEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
 /**
  * Product categories, like bakery, meat, etc.
  *
  * @author Anton Borovyk
  */
+
+@Data
+@AllArgsConstructor
 public class ProductCategory implements IDomainEntity<ProductCategory> {
 
     private final long categoryId;
     private String name;
 
-    public ProductCategory(long categoryId, String name) {
-        this.categoryId = categoryId;
-        this.name = name;
-    }
-
-    public long getCategoryId() {
-        return categoryId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     @Override
     public boolean sameIdentityAs(ProductCategory other) {
         return categoryId == other.categoryId;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        ProductCategory category = (ProductCategory) o;
-
-        if (categoryId != category.categoryId) return false;
-        return name != null ? name.equals(category.name) : category.name == null;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = (int) (categoryId ^ (categoryId >>> 32));
-        result = 31 * result + (name != null ? name.hashCode() : 0);
-        return result;
     }
 }
