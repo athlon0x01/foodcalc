1d56047e2568cba84ff3079e0815cbfdc3c0adc8
Olga Borovyk <pennerall@gmail.com>
2024-02-02 16:26:49 +0200

    roundToTwoDecimals added to FoodView

--- web-tier/src/main/java/com/outdoor/foodcalc/model/FoodView.java
+++ web-tier/src/main/java/com/outdoor/foodcalc/model/FoodView.java
@@ -1,10 +1,13 @@
 package com.outdoor.foodcalc.model;
 
-import lombok.Data;
+import lombok.*;
 import lombok.experimental.SuperBuilder;
 import lombok.extern.jackson.Jacksonized;
 
-@Data
+@Setter
+@ToString
+@EqualsAndHashCode
+@RequiredArgsConstructor
 @Jacksonized
 @SuperBuilder
 public class FoodView {
@@ -14,4 +17,32 @@ public class FoodView {
     private float fats;
     private float carbs;
     private float weight;
+
+    public long getId() {
+        return id;
+    }
+
+    public float getCalorific() {
+        return roundToTwoDecimals(calorific);
+    }
+
+    public float getProteins() {
+        return roundToTwoDecimals(proteins);
+    }
+
+    public float getFats() {
+        return roundToTwoDecimals(fats);
+    }
+
+    public float getCarbs() {
+        return roundToTwoDecimals(carbs);
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    private float roundToTwoDecimals(float number) {
+        return Math.round(number * 100)/100f;
+    }
 }
